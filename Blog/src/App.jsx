486c0204061c548c7175069754ec44fd63d14ca5
Vaibhav Gawas <vaibhavgawas343@gmail.com>
2025-12-27 20:16:09 +0530

    configuring the redux toolkit

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import {Header,Footer} from './components/index'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <div>
-        <h1>Bloggin Website</h1>
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({ userData }));
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-700'>
+      <div className='w-full block'>
+        <Header/>
+        <Footer/>
       </div>
-    </>
-  )
+    </div>
+  ) : null
 }
 
 export default App
